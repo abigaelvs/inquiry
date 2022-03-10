db8d9b87435fc7c27e8994426036241d40bcd05c
abigaelvs <abigaelvas@gmail.com>
2022-03-10 13:33:41 +0700

    Add ParameterController for testing

---
+++ Controllers/ParameterController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Text.Json;
+using System.Collections.Generic;
+
+using CNDS.SqlPaging;
+
+using InqService.Entity;
+using InqService.Repository;
+
+namespace InqService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ParameterController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult GetParam([FromQuery] SQLPage page,
+            [FromBody] JsonElement requestBody)
+        {
+            List<ParameterLevel1> result = ParameterRepository.GetPageParameterLvl1(page);
+            return Ok(result);
+        }
+    }
+}
