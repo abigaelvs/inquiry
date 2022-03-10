54bc859f298ccde0d07261bd2d8b5f02dc6e191a
abigaelvs <abigaelvas@gmail.com>
2022-03-10 18:10:45 +0700

    Add CustomerRequest

---
+++ Model/CustomerRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace InqService.Model
+{
+    public class CustomerRequest : StandardMessage
+    {
+        public string Oid { get; set; }
+
+        [JsonPropertyName("cust_name")]
+        public string CustName { get; set; }
+    }
+}
