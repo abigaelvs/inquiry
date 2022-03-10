95b891b46ea510f3a876130bfe2b082f6d3242fd
abigaelvs <abigaelvas@gmail.com>
2022-03-10 18:39:11 +0700

    Add Paging

---
+++ Model/Page.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace InqService.Model
+{
+    public class Page
+    {
+        [JsonPropertyName("page_no")]
+        public int PageNo { get; set; }
+
+        [JsonPropertyName("total_page")]
+        public int TotalPage { get; set; }
+
+        [JsonPropertyName("total_row")]
+        public int TotalRow { get; set; }
+
+        [JsonPropertyName("row_per_page")]
+        public int RowPerPage { get; set; }
+    }
+}
