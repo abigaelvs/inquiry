c2f6c7409dfeb8c41db064e5d107f466f6a92034
abigaelvs <abigaelvas@gmail.com>
2022-03-10 17:02:53 +0700

    Add CustomerRepository

---
+++ Repository/CustomerRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CNDS.Connection;
+using CNDS.SqlPaging;
+using CNDS.SqlStandard;
+using CNDS.Utils;
+
+using InqService.Constants;
+using InqService.Entity;
+using InqService.Model;
+
+namespace InqService.Repository
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public CustomerResponse GetCustomers(SQLPage page)
+        {
+            DBConnection dbconn = null;
+            List<MsCustomer> customers = null;
+
+            try
+            {
+                dbconn = new DBConnection();
+                SQLStandard sql = new SQLStandard(dbconn);
+                dbconn.BeginTransaction();
+
+                page.RowsPerPage = Int16.Parse(
+                    StartupRepository.ht[GeneralConstant.ParameterRowPerPage].ToString());
+                customers = sql.ExecuteQueryPaging<MsCustomer>(MsCustomer.TableName,
+                    null, null, null, page);
+
+                dbconn.CommitTransaction();
+            }
+            catch (Exception ex)
+            {
+                if (dbconn != null) dbconn.Rollback();
+            }
+            finally
+            {
+                if (dbconn != null) dbconn.Close();
+            }
+
+            CustomerResponse resp = new CustomerResponse
+            {
+                ResponseCode = ResponseCodeConstant.RcSuccess,
+                ResponseDesc = ResponseCodeConstant.MsgSuccess,
+                RequestTime = DateTime.Now.ToString(),
+                ChannelId = "Channel Id",
+                SourceReffId = DateTime.Now,
+                PageNo = page.PageNo,
+                CustomerList = customers
+            };
+            return resp;
+        }
+    }
+}
---
+++ Repository/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+using CNDS.SqlPaging;
+
+using InqService.Entity;
+using InqService.Model;
+
+namespace InqService.Repository
+{
+    public interface ICustomerRepository
+    {
+        CustomerResponse GetCustomers(SQLPage page);
+    }
+}
