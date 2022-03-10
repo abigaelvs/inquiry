2c678682ded75b0df90dba7a2f129ef51e9b834a
abigaelvs <abigaelvas@gmail.com>
2022-03-10 17:02:33 +0700

    Add StandardMessage as template for request and response

---
+++ Model/StandardMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace InqService.Model
+{
+    public class StandardMessage
+    {
+        [JsonPropertyName("response_code")]
+        public string ResponseCode { get; set; }
+
+        [JsonPropertyName("response_desc")]
+        public string ResponseDesc { get; set; }
+
+        [JsonPropertyName("request_time")]
+        public string RequestTime { get; set; }
+
+        [JsonPropertyName("channel_id")]
+        public string ChannelId { get; set; }
+
+        [JsonPropertyName("source_reff_id")]
+        public DateTime SourceReffId { get; set; }
+
+        [JsonPropertyName("page_no")]
+        public int PageNo { get; set; }
+    }
+}
