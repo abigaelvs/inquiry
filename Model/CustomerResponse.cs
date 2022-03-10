5d13bfb5a081ce6d64ab0ea38f97b74714484ec2
abigaelvs <abigaelvas@gmail.com>
2022-03-10 17:02:45 +0700

    Add CustomerResponse for MsCustomer

---
+++ Model/CustomerResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+using InqService.Entity;
+
+namespace InqService.Model
+{
+    public class CustomerResponse : StandardMessage
+    {
+        [JsonPropertyName("customer_list")]
+        public List<MsCustomer> CustomerList { get; set; }
+    }
+}
