4eb3c7cf8e83848a5a4fd6fddf8104ac984167da
abigaelvs <abigaelvas@gmail.com>
2022-03-11 16:38:36 +0700

    Add constructor

--- Model/CustomerResponse.cs
+++ Model/CustomerResponse.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
+using CNDS.SqlPaging;
+
 using InqService.Entity;
+using InqService.Model;
 
 namespace InqService.Model
 {
@@ -9,5 +13,21 @@ namespace InqService.Model
     {
         [JsonPropertyName("customer_list")]
         public List<MsCustomer> CustomerList { get; set; }
+
+        public CustomerResponse(string responseCode, string responseDesc, 
+            CustomerRequest customer, SQLPage page, List<MsCustomer> customers)
+            : base(responseCode, responseDesc, customer.RequestTime, customer.ChannelId,
+                customer.SourceReffId, customer.ReffId, page)
+        {
+            CustomerList = customers;
+        }
+
+        public CustomerResponse(string responseCode, string responseDesc,
+            StandardMessage customer, SQLPage page, List<MsCustomer> customers)
+            : base(responseCode, responseDesc, customer.RequestTime, customer.ChannelId,
+                customer.SourceReffId, customer.ReffId, page)
+        {
+            CustomerList = customers;
+        }
     }
 }
--- Model/StandardMessage.cs
+++ Model/StandardMessage.cs
@@ -16,6 +16,9 @@ namespace InqService.Model
         [JsonPropertyName("request_time")]
         public string RequestTime { get; set; }
 
+        [JsonPropertyName("response_time")]
+        public string ResponseTime { get; set; }
+
         [JsonPropertyName("channel_id")]
         public string ChannelId { get; set; }
 
@@ -30,5 +33,31 @@ namespace InqService.Model
         public int PageNo { get; set; }
 
         public SQLPage Paging { get; set; }
+
+        public StandardMessage() { }
+        public StandardMessage(string responseCode, string responseDesc, string requestTime,
+            string channelId, string sourceReffId, string reffId)
+        {
+            ResponseCode = responseCode;
+            ResponseDesc = responseDesc;
+            RequestTime = requestTime;
+            ResponseTime = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+            ChannelId = channelId;
+            SourceReffId = sourceReffId;
+            ReffId = reffId;
+        }
+
+        public StandardMessage(string responseCode, string responseDesc, string requestTime,
+            string channelId, string sourceReffId, string reffId, SQLPage page)
+        {
+            ResponseCode = responseCode;
+            ResponseDesc = responseDesc;
+            RequestTime = requestTime;
+            ResponseTime = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+            ChannelId = channelId;
+            SourceReffId = sourceReffId;
+            ReffId = reffId;
+            Paging = page;
+        }
     }
 }
