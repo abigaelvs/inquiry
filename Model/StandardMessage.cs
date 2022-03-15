655316f21309cdaaeffefa01e2292e54761a5f30
abigaelvs <abigaelvas@gmail.com>
2022-03-15 12:24:57 +0700

    Add NoCopyProperty

--- Model/StandardMessage.cs
+++ Model/StandardMessage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text.Json.Serialization;
 
+using CNDS.Utils;
 using CNDS.SqlPaging;
 
 namespace InqService.Model
@@ -8,15 +9,18 @@ namespace InqService.Model
     public class StandardMessage
     {
         [JsonPropertyName("response_code")]
+        [NoCopyProperty]
         public string ResponseCode { get; set; }
 
         [JsonPropertyName("response_desc")]
+        [NoCopyProperty]
         public string ResponseDesc { get; set; }
 
         [JsonPropertyName("request_time")]
         public string RequestTime { get; set; }
 
         [JsonPropertyName("response_time")]
+        [NoCopyProperty]
         public string ResponseTime { get; set; }
 
         [JsonPropertyName("channel_id")]
@@ -30,33 +34,19 @@ namespace InqService.Model
 
         [JsonPropertyName("page_no")]
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [NoCopyProperty]
         public int PageNo { get; set; }
 
+        [NoCopyProperty]
         public SQLPage Paging { get; set; }
 
         public StandardMessage() { }
-        public StandardMessage(string responseCode, string responseDesc, string requestTime,
-            string channelId, string sourceReffId, string reffId)
-        {
-            ResponseCode = responseCode;
-            ResponseDesc = responseDesc;
-            RequestTime = requestTime;
-            ResponseTime = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
-            ChannelId = channelId;
-            SourceReffId = sourceReffId;
-            ReffId = reffId;
-        }
 
-        public StandardMessage(string responseCode, string responseDesc, string requestTime,
-            string channelId, string sourceReffId, string reffId, SQLPage page)
+        public StandardMessage(string responseCode, string responseDesc, SQLPage page)
         {
             ResponseCode = responseCode;
             ResponseDesc = responseDesc;
-            RequestTime = requestTime;
             ResponseTime = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
-            ChannelId = channelId;
-            SourceReffId = sourceReffId;
-            ReffId = reffId;
             Paging = page;
         }
     }
